166368eaf0dd0c06c3f6a1a57afe338088a506cd
Benjamin Carter <benj.james.carter@gmail.com>
2022-02-17 16:00:34 -0800

    continued work on new location button and function

--- js/app.js
+++ js/app.js
@@ -86,6 +86,7 @@ Location.prototype.renderTable = function () {
   let bottomRow = document.createElement('tr');
   table.append(bottomRow);
   totalHead.textContent = totalsLabel;
+  // bottomRow.setAttribute("id","totalsRow");
   bottomRow.append(totalHead);
 
   let grandTotal = 0;
@@ -97,6 +98,7 @@ Location.prototype.renderTable = function () {
     grandTotal += total;
     let cellData = document.createElement('td');
     cellData.textContent = total;
+
     bottomRow.append(cellData);
   }
   let grandTotalCell = document.createElement('td');
@@ -116,7 +118,7 @@ buildLoc('Tokyo', 3, 24, 1.2);
 buildLoc('Dubai', 11, 38, 3.7);
 buildLoc('Paris', 20, 38, 2.3);
 buildLoc('Lima', 2, 16, 4.6);
-storeLocations[2].renderTable();
+storeLocations[0].renderTable();
 
 // code for my new locations form is below this line
 // step one --- grab element to listen too, in this case it is the Form stores as locForm
@@ -124,18 +126,37 @@ let locForm = document.getElementById('loc-Form');
 
 function appendNewLoc(event) {
   event.preventDefault();
-  console.log('This knows you hit the submit button!');
-  console.log(locForm);
 
   let formLocInput = event.target.location.value; // tested and working Returning numbers
   let formMinInput = parseInt(event.target.minHourlyCust.value);
   let formMaxInput = parseInt(event.target.maxHourlyCust.value);
   let formAveCookie = parseInt(event.target.aveCookiePer.value);
+  // buildLoc constructs new city and runs necessary prototypes to complete data in object
+  buildLoc(formLocInput, formMinInput, formMaxInput, formAveCookie);
+  //let index = storeLocations.length;
+  // delete totals row from bottom of table
+  // create new row with new city data (can use existing logic to make row)
+  // re run the render logic for the totals row.
 
-  console.log(formLocInput);
-  console.log(formMinInput);
-  console.log(formMaxInput);
-  console.log(formAveCookie);
+  let totalsRow = document.getElementById("cookieSales");
+  totalsRow.deleteRow(-1); // removes footer row / totals
+
+  let newStore = storeLocations.length - 1; // gives me index location of new object in array
+  // console.log(newStore);
+  let citiesRow = document.createElement('tr'); // create a new row on bottom of table
+  table.appendChild(citiesRow);
+
+  let locHead = document.createElement('th');
+  locHead.textContent = formLocInput;
+  citiesRow.append(locHead); // Adds new city name to left of table
+  
+  for (let i = 0; i < storeHours.length; i++) { // fills table with cookie sales data for new city
+    let cookieData = storeLocations[newStore].cookiesSoldPerHour[i];
+    let dataElem = document.createElement('td');
+    dataElem.textContent = cookieData;
+    citiesRow.appendChild(dataElem);
+  }
+
 }
 
 
