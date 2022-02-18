4582fdf5a1ec7be21d38daef1b1765e87d90ea0f
Benjamin Carter <benj.james.carter@gmail.com>
2022-02-17 16:09:42 -0800

    reformatted doc

--- js/app.js
+++ js/app.js
@@ -120,63 +120,52 @@ buildLoc('Paris', 20, 38, 2.3);
 buildLoc('Lima', 2, 16, 4.6);
 storeLocations[0].renderTable();
 
-// code for my new locations form is below this line
-// step one --- grab element to listen too, in this case it is the Form stores as locForm
 let locForm = document.getElementById('loc-Form');
 
 function appendNewLoc(event) {
   event.preventDefault();
 
-  let formLocInput = event.target.location.value; // tested and working Returning numbers
+  let formLocInput = event.target.location.value;
   let formMinInput = parseInt(event.target.minHourlyCust.value);
   let formMaxInput = parseInt(event.target.maxHourlyCust.value);
   let formAveCookie = parseInt(event.target.aveCookiePer.value);
-  // buildLoc constructs new city and runs necessary prototypes to complete data in object
+
   buildLoc(formLocInput, formMinInput, formMaxInput, formAveCookie);
-  //let index = storeLocations.length;
-  // delete totals row from bottom of table
-  // create new row with new city data (can use existing logic to make row)
-  // re run the render logic for the totals row.
 
-  let totalsRow = document.getElementById("cookieSales");
-  totalsRow.deleteRow(-1); // removes footer row / totals
+  let totalsRow = document.getElementById('cookieSales');
+  totalsRow.deleteRow(-1);
 
-  let newStore = storeLocations.length - 1; // gives me index location of new object in array
-  // console.log(newStore);
-  let citiesRow = document.createElement('tr'); // create a new row on bottom of table
+  let newStore = storeLocations.length - 1;
+  let citiesRow = document.createElement('tr');
   table.appendChild(citiesRow);
 
   let locHead = document.createElement('th');
   locHead.textContent = formLocInput;
-  citiesRow.append(locHead); // Adds new city name to left of table
-  
-  for (let i = 0; i < storeHours.length; i++) { // fills table with cookie sales data for new city
+  citiesRow.append(locHead);
+
+  for (let i = 0; i < storeHours.length; i++) {
     let cookieData = storeLocations[newStore].cookiesSoldPerHour[i];
     let dataElem = document.createElement('td');
     dataElem.textContent = cookieData;
     citiesRow.appendChild(dataElem);
   }
 
-  let salesTotal = storeLocations[newStore].totalCookies; // fills in the daily totals
+  let salesTotal = storeLocations[newStore].totalCookies;
   let salesData = document.createElement('td');
   salesData.textContent = salesTotal;
   citiesRow.appendChild(salesData);
 
-
-
-
-  let totalHead = document.createElement('th'); // creates the hourly totals row for all locations
+  let totalHead = document.createElement('th');
   let totalsLabel = 'Totals';
   let bottomRow = document.createElement('tr');
   table.append(bottomRow);
   totalHead.textContent = totalsLabel;
-  // bottomRow.setAttribute("id","totalsRow");
   bottomRow.append(totalHead);
 
   let grandTotal = 0;
-  for (let i = 0; i < storeHours.length; i++) { // slow loop moves to the right of the table with each loop
+  for (let i = 0; i < storeHours.length; i++) {
     let total = 0;
-    for (let j = 0; j < storeLocations.length; j++) { // fast loop, moves down through the stores, totally the cookie sales for each hour.
+    for (let j = 0; j < storeLocations.length; j++) {
       total += storeLocations[j].cookiesSoldPerHour[i];
     }
     grandTotal += total;
